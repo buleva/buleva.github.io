bbb3e73e9d25ad56484d8417dff0f49ae84d1c03
Buleva <valentin.buchner@icloud.com>
2023-05-13 10:33:46 +0200

    added image files

--- src/scripts.js
+++ src/scripts.js
@@ -1,73 +1,73 @@
-var firstToneEx1;
-var secondToneEx1;
-let toneLengthEx1 = 0.7;
-let pauseLengthEx1 = 0.2;
-var answerGiven = false;
-var buttonDelay = 0;
-var lastClick = 0;
-var oscType;
-var currentHTMLFile;
+var firstTone;
+var secondTone;
+let toneLength = 0.7;
+let pauseLength = 0.2;
+var oscType = "sine";
 var soundeffectCorrect;
 var soundeffectWrong;
 let intervalArray = [0.9707, 1.0293];
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+var answerGiven = false;
+var buttonDelay = 0;
+var lastClick = 0;
+var soundeffectCorrect = new Audio('./sounds/correct.wav');
+var soundeffectWrong = new Audio('./sounds/wrong.wav');
 
 
-function playExercise1(freq, start, stop, fadeInTime, fadeOutTime) {
+function play(freq, start, stop, fadeInTime, fadeOutTime) {
         const osc = audioContext.createOscillator();
         const gainNode = audioContext.createGain();
-        let outputGain;
-        outputGain = localStorage.getItem('volume')/100;
         osc.connect(gainNode).connect(audioContext.destination);
         osc.frequency.value = freq;
-        osc.type = "sine";
+        osc.type = oscType;
         gainNode.gain.value = 0.001;
         osc.start(audioContext.currentTime + start);
         gainNode.gain.linearRampToValueAtTime(0.001, audioContext.currentTime + start);
-        gainNode.gain.exponentialRampToValueAtTime(outputGain, audioContext.currentTime + start + fadeInTime);
-        gainNode.gain.linearRampToValueAtTime(outputGain, audioContext.currentTime + stop - fadeOutTime)
+        gainNode.gain.exponentialRampToValueAtTime(1, audioContext.currentTime + start + fadeInTime);
+        gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + stop - fadeOutTime)
         gainNode.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + stop);
         osc.stop(audioContext.currentTime + stop);
 }
 
-
-function nextExercise1() {
+function next() {
     if (lastClick <= Date.now()-buttonDelay) { 
-        firstToneEx1 = getRndInteger(100,400); //define good range of freqs
-        secondToneEx1 = getRndFromArray(intervalArray)*firstToneEx1;
-        document.getElementById('feedback_img').src = '../pictures/questionmark.png';
-        playExercise1(firstToneEx1, 0, toneLengthEx1, 0.05, 0.05);
-        playExercise1(secondToneEx1, toneLengthEx1+pauseLengthEx1, 2*toneLengthEx1+pauseLengthEx1, 0.05, 0.05)
+        firstTone = getRndInteger(100,400); //define good range of freqs
+        secondTone = getRndFromArray(intervalArray)*firstTone;
+        document.getElementById('feedback_img').src = './images/questionmark.png';
+        play(firstTone, 0, toneLength, 0.05, 0.05);
+        play(secondTone, toneLength+pauseLength, 2*toneLength+pauseLength, 0.05, 0.05);
         answerGiven = false;
         lastClick = Date.now();
-        buttonDelay = (2*toneLengthEx1+pauseLengthEx1)*1000;
-        localStorage.setItem('firstTone', firstToneEx1);
-        localStorage.setItem('secondTone', secondToneEx1);
+        buttonDelay = (2*toneLength+pauseLength)*1000;
+        localStorage.setItem('firstTone', firstTone);
+        localStorage.setItem('secondTone', secondTone);
     }
 }
 
-function repeatExercise1() {
+function repeat() {
     if (lastClick <= Date.now()-buttonDelay) {
-        firstToneEx1 = localStorage.getItem('firstToneEx1');
-        secondToneEx1 = localStorage.getItem('secondToneEx1');
-        playExercise1(firstToneEx1, oscType, 0, toneLengthEx1, 0.05, 0.05);
-        playExercise1(secondToneEx1, oscType, toneLengthEx1+pauseLengthEx1, 2*toneLengthEx1+pauseLengthEx1, 0.05, 0.05)        
+        if (localStorage.getItem('firstTone') == '') {localStorage.setItem('firstTone', 250)}
+        if (localStorage.getItem('secondTone') == '') {localStorage.setItem('secondTone', 242.675)}
+        firstTone = localStorage.getItem('firstTone');
+        secondTone = localStorage.getItem('secondTone');
+        play(firstTone, 0, toneLength, 0.05, 0.05);
+        play(secondTone, toneLength+pauseLength, 2*toneLength+pauseLength, 0.05, 0.05);        
         lastClick = Date.now();
-        buttonDelay = (2*toneLengthEx1+pauseLengthEx1)*1000;
+        buttonDelay = (2*toneLength+pauseLength)*1000;
         if (!answerGiven) {
-            document.getElementById('feedback_img').src = '../pictures/questionmark.png';
+            document.getElementById('feedback_img').src = './images/questionmark.png';
         }
     }
 }
 
-
-// answer buttons
 function higher() {
     if (!answerGiven) {
-        if (firstToneEx1 < secondToneEx1) {
-            document.getElementById('feedback_img').src = '../pictures/correct.png';
-            playCorrectSoundeffect();
+        if (firstTone < secondTone) {
+            document.getElementById('feedback_img').src = './images/correct.png';
+            soundeffectCorrect.play();
         } else {
-            showCorrectAnswer(firstToneEx1, secondToneEx1);
+            showCorrectAnswer(firstTone, secondTone);
         }
         answerGiven = true;
     }
@@ -75,43 +75,15 @@ function higher() {
 
 function lower() {
     if (!answerGiven) {
-        if (firstToneEx1 > secondToneEx1) {
-            document.getElementById('feedback_img').src = '../pictures/correct.png';
-            playCorrectSoundeffect();
+        if (firstTone > secondTone) {
+            document.getElementById('feedback_img').src = './images/correct.png';
+            soundeffectCorrect.play();
         } else {
-            showCorrectAnswer(firstToneEx1, secondToneEx1);
+            showCorrectAnswer(firstTone, secondTone);
         }
         answerGiven = true;
     }
 }
-
-
-
-
-// initialize variables
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext();
-var answerGiven = false;
-var buttonDelay = 0;
-var lastClick = 0;
-var soundeffectCorrect = new Audio('../sounds/correct.wav');
-var soundeffectWrong = new Audio('../sounds/wrong.wav');
-
-// finish js look through!!
-
-function updateButton(button_id, design) {
-    button = document.getElementById(button_id);
-    if (design=='active') {
-        button.style.backgroundColor = 'white';
-        button.style.color = 'var(--cyan)';
-    } else {
-        button.style.backgroundColor = '';
-        button.style.color = '';
-    }
-}
-
-
-// outsourced functions
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -121,23 +93,11 @@ function getRndFromArray(Array) {
     return Array[Math.floor(Math.random()*Array.length)];
 }
 
-
 function showCorrectAnswer(firstTone, secondTone) {
     if (firstTone > secondTone) {
-        document.getElementById('feedback_img').src = '../pictures/lower.png';
-    } else if (firstTone == secondTone) {
-        document.getElementById('feedback_img').src = '../pictures/equal.png';
+        document.getElementById('feedback_img').src = './images/lower.png';
     } else {
-        document.getElementById('feedback_img').src = '../pictures/higher.png';
+        document.getElementById('feedback_img').src = './images/higher.png';
     }
-    if (localStorage.getItem('soundeffects')=='true') {
-        soundeffectWrong.volume = localStorage.getItem('volume')/100;
-        soundeffectWrong.play();
-    }
+    soundeffectWrong.play();
 }
-
-function playCorrectSoundeffect() {
-    soundeffectCorrect.volume = localStorage.getItem('volume')/100;
-    soundeffectCorrect.play();
-}
-
